40caa81dadfd310c941170858a523328433b4300
AlurrAldrey <48604870+AlurrAldrey@users.noreply.github.com>
2021-05-05 15:18:30 -0300

    Archivos de prueba y ódigo final

--- contrato.py
+++ contrato.py
@@ -1,12 +1,10 @@
+#definición de la clase contrato para simplificar las operaciones del algoritmo
 class Contrato:
     
     def __init__(self,nombre,inicio,fin):
         self.nombre = nombre
         self.inicio = inicio
         self.fin = fin
-
-    def __str__(self):
-        return (self.nombre + "," + self.inicio + "," + self.fin)
 
 def get_fin_contrato(c):
     return c.fin
